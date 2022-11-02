903b871128b7df6e2803fbf448392bcee831c26f
Yu Yang Quek <quekyuyang@gmail.com>
2022-11-02 21:31:31 +0100

    Move client side top level logic to Game class and make unit tests for it

--- public/chess.js
+++ public/chess.js
@@ -1,94 +1,11 @@
-import {createPickupEvent} from "./UI.js"
-import {init, update} from "./render.js"
-import {SpriteManager} from "./SpriteManager.js"
-
-function create_piece_sprite(img_path, id) {
-  let sprite = new Image();
-  sprite.src = img_path;
-  sprite.classList.add('chess-piece');
-  sprite.id = id;
-  sprite.draggable = false;
-  return sprite;
-}
+import {Game} from "./Game.js"
 
 
-function get_img_path(type, player_n) {
-  let img_path = "images/";
-  img_path += player_n == 1 ? "w_" : "b_";
-  img_path = img_path + type + ".png";
-  return img_path;
-}
-
-let sprite_manager = null;
-let moves = null;
-
-function move_piece(id, x, y) {
-  send_move_to_server(id, x, y)
-  .then((response) => response.json())
-  .then(function (data) {
-    if (data.success) {
-      for (let id in moves) {
-        delete moves[id];
-      }
-      Object.assign(moves, data.moves);
-      update(data.chessboard, data.graveyard);
-      sprite_manager.disable_move();
-      wait_for_update();
-    }
-  });
-}
-
-function wait_for_update() {
-  return fetch('http://127.0.0.1:3000/game/match_state')
-  .then((response) => response.json())
-  .then((state) => {
-    moves = state.moves;
-    update(state.chessboard, state.graveyard);
-    sprite_manager.enable_move(state.moves);
-  });
-}
-
-function send_move_to_server(id, x, y) {
-  return fetch('http://127.0.0.1:3000/game/move_piece', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      id: id,
-      x: x,
-      y: y
-    })
-  });
-}
-
-
-function main() {
-  fetch('http://127.0.0.1:3000/new_match')
+function get_match_data() {
+  return fetch('http://127.0.0.1:3000/new_match')
   .then(() => fetch('http://127.0.0.1:3000/game/valid_moves'))
   .then((response) => response.json())
-  .then(function (data) {
-    init();
-    const chessboard = data.chessboard.flat();
-    let squares = document.querySelectorAll("div.chessboard div");
-    const sprites = [];
-    for (let i = 0; i < chessboard.length; i++) {
-      if (chessboard[i]) {
-        let chesspiece = chessboard[i];
-        let img_path = get_img_path(chesspiece.move_type, chesspiece.player);
-        let sprite = create_piece_sprite(img_path, chesspiece.id);
-        sprites[chesspiece.id] = sprite;
-        squares[i].append(sprite);
-      }
-    }
-    update(data.chessboard, data.graveyard);
-    sprite_manager = new SpriteManager(sprites, move_piece);
-    if (data.first_move)
-    {
-      moves = data.moves;
-      sprite_manager.enable_move(data.moves);
-    }
-    else
-      wait_for_update();
-  });
 }
 
-main();
+const game = new Game();
+game.init(get_match_data);
---
+++ tests/Game.test.js
@@ -0,0 +1,81 @@
+import {Game} from "../public/Game.js"
+import {init, update} from "../public/render.js"
+import {SpriteManager} from "../public/SpriteManager.js"
+import {send_move_to_server, get_match_state} from "../public/server_comms.js"
+
+
+jest.mock("../public/render.js")
+jest.mock("../public/SpriteManager.js")
+
+beforeEach(() => {
+  SpriteManager.mockClear()
+})
+
+test("Start game with first move", async () => {
+  const match_state = {
+    chessboard: [],
+    graveyard: null,
+    first_move: true,
+    moves: "moves"
+  }
+  const game = new Game()
+  await game.init(() => Promise.resolve(match_state))
+
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls.length).toBe(1)
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls[0][0])
+  .toBe(match_state.moves)
+})
+
+
+jest.mock("../public/server_comms.js")
+
+test("Start game without first move", async () => {
+  const new_match_state = {
+    chessboard: [],
+    graveyard: [],
+    moves: "moves"
+  }
+  get_match_state.mockResolvedValue(new_match_state)
+
+  const init_match_state = {
+    chessboard: [],
+    graveyard: null,
+    first_move: false,
+  }
+  const game = new Game()
+  await game.init(() => Promise.resolve(init_match_state))
+
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls.length).toBe(1)
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls[0][0])
+  .toBe(new_match_state.moves)
+})
+
+
+test("Move piece then wait for opponent", async () => {
+  const match_state1 = {
+    chessboard: [],
+    graveyard: null,
+    first_move: true,
+    moves: "moves1"
+  }
+  const game = new Game()
+  await game.init(() => Promise.resolve(match_state1))
+
+  const match_state2 = {
+    chessboard: [],
+    graveyard: [],
+    success: true
+  }
+  send_move_to_server.mockResolvedValue(match_state2)
+  const match_state3 = {
+    chessboard: [],
+    graveyard: [],
+    moves: "moves3"
+  }
+  get_match_state.mockResolvedValue(match_state3)
+
+  await game.move_piece('id', 1, 1)
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls.length).toBe(2)
+  expect(SpriteManager.mock.instances[0].enable_move.mock.calls[1][0])
+  .toBe(match_state3.moves)
+})
