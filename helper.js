49099eef19a6c602d44ce9b02b97bcb068622dd1
Yu Yang Quek <quekyuyang@gmail.com>
2022-11-07 10:54:54 +0100

    Move toString call

--- helper.js
+++ helper.js
@@ -8,9 +8,10 @@ function queue_match(req, res, database_interface) {
   else {
     const match_id = database_interface.new_match(
       waiting.req.session.id, req.session.id
-    );
-    req.session.match_id = match_id.toString();
-    waiting.req.session.match_id = match_id.toString();
+    ).toString();
+    req.session.match_id = match_id;
+    waiting.req.session.match_id = match_id;
+
     res.json({opponent: waiting.req.session.id});
     waiting.res.json({opponent: req.session.id});
     waiting = null;
