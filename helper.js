7e306dec2ec7ef32e2b8051c690d02e2470b2e88
Yu Yang Quek <quekyuyang@gmail.com>
2022-11-07 21:02:01 +0100

    Pending request empyting was in async code which might not execute before more requests are processed. Moved it to synchronous part of code

--- helper.js
+++ helper.js
@@ -6,33 +6,35 @@ function queue_match(req, res, database_interface) {
     waiting = {req: req, res: res};
   }
   else {
+    const req_waiting = waiting;
+    waiting = null;
+    
     const match_id = database_interface.new_match(
-      waiting.req.session.id, req.session.id
+      req_waiting.req.session.id, req.session.id
     ).toString();
     req.session.match_id = match_id;
-    waiting.req.session.match_id = match_id;
+    req_waiting.req.session.match_id = match_id;
 
     database_interface.find_match(match_id)
     .then(match => {
       // Determine whether player of current request gets first move
       const first_move_current = req.session.id == match.player_ids[0]
       // Determine whether player of waiting request gets first move
-      const first_move_waiting = waiting.req.session.id == match.player_ids[0]
+      const first_move_waiting = req_waiting.req.session.id == match.player_ids[0]
 
       res.json({
         ...match,
         first_move: first_move_current
       })
-      waiting.res.json({
+      req_waiting.res.json({
         ...match,
         first_move: first_move_waiting
       })
-      waiting = null;
     })
     .catch(error => {
       console.log(error);
       res.sendStatus(500);
-      waiting.res.sendStatus(500);
+      req_waiting.res.sendStatus(500);
       waiting = null;
     });
   }
--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,45 +1,73 @@
 const queue_match = require("../helper.js")
 
-test("Test matchmaking", async () => {
-  const req1 = {
-    session: {
-      id: 'id1'
-    }
-  }
-  const req2 = {
-    session: {
-      id: 'id2'
-    }
+
+class FakeReqGenerator {
+  constructor() {
+    this.count = 0
   }
 
-  const res1 = {
-    json: jest.fn()
+  new_req() {
+    const req = {
+      session: { id: 'id' + this.count.toString() }
+    }
+    this.count++
+    return req
   }
-  const res2 = {
-    json: jest.fn()
-  }
+}
 
-  const fake_database_interface = {}
-  const match_id = 'match_id'
-  fake_database_interface.new_match = jest.fn(() => match_id)
-  const fake_match_data = {
+function create_fake_match_data(id1, id2) {
+  return {
     chessboard: [],
     graveyard: [],
     moves: {},
-    player_ids: [req1.session.id, req2.session.id]
+    player_ids: [id1, id2]
   }
-  fake_database_interface.find_match = jest.fn(() => Promise.resolve(fake_match_data))
+}
+
+
+test("Test matchmaking queue", async () => {
+  const req_generator = new FakeReqGenerator()
+  const req1 = req_generator.new_req()
+  const req2 = req_generator.new_req()
+  const req3 = req_generator.new_req()
+  const req4 = req_generator.new_req()
+
+  const res1 = { json: jest.fn() }
+  const res2 = { json: jest.fn() }
+  const res3 = { json: jest.fn() }
+  const res4 = { json: jest.fn() }
+
+  const fake_database_interface = {}
+  const match_id1 = 'match_id1'
+  const match_id2 = 'match_id2'
+  fake_database_interface.new_match = jest.fn()
+  .mockReturnValueOnce(match_id1).mockReturnValueOnce(match_id2)
+  
+  const fake_match_data1 = create_fake_match_data(req1.session.id, req2.session.id)
+  const fake_match_data2 = create_fake_match_data(req3.session.id, req4.session.id)
+  fake_database_interface.find_match = jest.fn()
+  .mockResolvedValueOnce(fake_match_data1).mockResolvedValueOnce(fake_match_data2)
 
   queue_match(req1, res1, fake_database_interface)
   await queue_match(req2, res2, fake_database_interface)
+  queue_match(req3, res3, fake_database_interface)
+  await queue_match(req4, res4, fake_database_interface)
 
-  expect(fake_database_interface.new_match.mock.calls.length).toBe(1)
+  expect(fake_database_interface.new_match.mock.calls.length).toBe(2)
   expect(fake_database_interface.new_match.mock.calls[0][0]).toBe(req1.session.id)
   expect(fake_database_interface.new_match.mock.calls[0][1]).toBe(req2.session.id)
-  expect(req1.session.match_id).toBe(match_id)
-  expect(req2.session.match_id).toBe(match_id)
+  expect(fake_database_interface.new_match.mock.calls[1][0]).toBe(req3.session.id)
+  expect(fake_database_interface.new_match.mock.calls[1][1]).toBe(req4.session.id)
+  expect(req1.session.match_id).toBe(match_id1)
+  expect(req2.session.match_id).toBe(match_id1)
+  expect(req3.session.match_id).toBe(match_id2)
+  expect(req4.session.match_id).toBe(match_id2)
   expect(res1.json.mock.calls.length).toBe(1)
   expect(res2.json.mock.calls.length).toBe(1)
-  expect(res1.json.mock.calls[0][0]).toStrictEqual({...fake_match_data, first_move:true})
-  expect(res2.json.mock.calls[0][0]).toStrictEqual({...fake_match_data, first_move:false})
+  expect(res3.json.mock.calls.length).toBe(1)
+  expect(res4.json.mock.calls.length).toBe(1)
+  expect(res1.json.mock.calls[0][0]).toStrictEqual({...fake_match_data1, first_move:true})
+  expect(res2.json.mock.calls[0][0]).toStrictEqual({...fake_match_data1, first_move:false})
+  expect(res3.json.mock.calls[0][0]).toStrictEqual({...fake_match_data2, first_move:true})
+  expect(res4.json.mock.calls[0][0]).toStrictEqual({...fake_match_data2, first_move:false})
 })
