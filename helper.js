69b91764266cd79bb111ee7e8baad5c9db195626
Yu Yang Quek <quekyuyang@gmail.com>
2023-03-02 07:29:34 +0100

    Separate out part of queueMatch function

--- helper.js
+++ helper.js
@@ -11,21 +11,9 @@ function queueMatch(req, res, database_interface) {
   else {
     const req_waiting = waiting
     waiting = null
-    
-    const chessboard = new Chessboard()
-    chessboard.init()
-    database_interface.newMatch(chessboard, req_waiting.req.session.id, req.session.id)
-    .then(match_id => {
-      req.session.match_id = match_id
-      req_waiting.req.session.match_id = match_id
-      const moveManager = new MoveManager(chessboard)
-      const matchInfo = {
-        chessboard: chessboard.chessboard,
-        graveyard: chessboard.graveyard,
-        moves: moveManager.compute_moves(1),
-        player_ids: [req_waiting.req.session.id, req.session.id]
-      }
 
+    return newMatch(req_waiting.req, req, database_interface)
+    .then(matchInfo => {
       // Determine whether player of current request gets first move
       const first_move_current = req.session.id == matchInfo.player_ids[0]
       // Determine whether player of waiting request gets first move
@@ -47,6 +35,25 @@ function queueMatch(req, res, database_interface) {
       })
     })
   }
+}
+
+
+function newMatch(req1, req2, database_interface) {
+  const chessboard = new Chessboard()
+  chessboard.init()
+  return database_interface.newMatch(chessboard, req1.session.id, req2.session.id)
+  .then(match_id => {
+    req1.session.match_id = match_id
+    req2.session.match_id = match_id
+    const moveManager = new MoveManager(chessboard)
+    const matchInfo = {
+      chessboard: chessboard.chessboard,
+      graveyard: chessboard.graveyard,
+      moves: moveManager.compute_moves(1),
+      player_ids: [req1.session.id, req2.session.id]
+    }
+    return matchInfo
+  })
 }
 
 
