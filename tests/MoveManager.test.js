c458e2c1a2c8761986dc1d0a54cff7bd117eba46
Yu Yang Quek <quekyuyang@gmail.com>
2023-03-15 06:27:14 +0100

    Rewrite MoveManager tests to not overuse mocks and instead test it's actually internal functions using it's public interface

--- tests/MoveManager.test.js
+++ tests/MoveManager.test.js
@@ -1,59 +1,31 @@
 const MoveManager = require('../chess/MoveManager')
 const Chessboard = require('../chess/Chessboard')
-const {Rook} = require('../chess/ChessPiece')
-const filterMoves = require('../chess/filterMoves')
 const Vector = require('../chess/Position');
-const Move = require('../chess/Move')
 
 
-jest.mock('../chess/Chessboard')
-jest.mock('../chess/base_moveset')
-jest.mock('../chess/filterMoves')
-
-test('Attempt to move non-existant chess piece should return fail', () => {
+test('Attempt to move non-existent chess piece should fail', () => {
     const chessboard = new Chessboard()
-    chessboard.chesspieces1 = []
+    chessboard.init()
     const moveManager = new MoveManager(chessboard)
 
-    const pos = {x: 0, y: 3}
-    const success = moveManager.move_piece('id1', pos, 1)
+    const chessboardRef = Chessboard.clone(chessboard)
+    const success = moveManager.move_piece('non-existent id', new Vector(0, 5), 1)
     expect(success).toBe(false)
-    expect(chessboard.move_piece).not.toHaveBeenCalled()
+    expect(chessboard).toEqual(chessboardRef)
 })
 
-test('Attempt to execute valid move should succeed', () => {
+test('Attempt to move valid chess piece should succeed', () => {
     const chessboard = new Chessboard()
-    const chesspiece = new Rook(1, 3, 1, 'id1')
-    chessboard.chesspieces1 = [chesspiece]
+    chessboard.init()
     const moveManager = new MoveManager(chessboard)
 
-    const dest = new Vector(0, 3)
-    const move = new Move(dest)
+    const chessboardRef = Chessboard.clone(chessboard)
+    const pawn = chessboardRef.chessboard[6][0]
+    pawn.pos = new Vector(0, 5)
+    chessboardRef.chessboard[5][0] = chessboardRef.chessboard[6][0]
+    chessboardRef.chessboard[6][0] = null
 
-    filterMoves.mockReturnValue({
-        'id1': [move]
-    })
-
-    const success = moveManager.move_piece('id1', dest, 1)
+    const success = moveManager.move_piece(pawn.id, new Vector(0, 5), 1)
     expect(success).toBe(true)
-    expect(chessboard.move_piece).toHaveBeenCalledWith(chesspiece.pos.y, chesspiece.pos.x, move)
-})
-
-test('Attempt to execute invalid move should return fail', () => {
-    const chessboard = new Chessboard()
-    const chesspiece = new Rook(1, 3, 1, 'id1')
-    chessboard.chesspieces1 = [chesspiece]
-    const moveManager = new MoveManager(chessboard)
-
-    const dest = new Vector(0, 3)
-    const move = new Move(dest)
-
-    filterMoves.mockReturnValue({
-        'id1': [move]
-    })
-
-    const invalidDest = new Vector(1, 3)
-    const success = moveManager.move_piece('id1', invalidDest, 1)
-    expect(success).toBe(false)
-    expect(chessboard.move_piece).not.toHaveBeenCalled()
+    expect(chessboard).toEqual(chessboardRef)
 })
